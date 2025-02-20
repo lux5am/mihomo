f93f1ed5661cd77dcabd13e7c3ca95d9732a61ee
lux5am <lux.5am@yandex.com>
2025-02-20 08:23:57 +0800

    feat: add `socks-prefer-host` to general config
    Prefer to send host in socks payload. True by default.
    
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- constant/metadata.go
+++ constant/metadata.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"net/netip"
 	"strconv"
+	"sync/atomic"
 )
 
 // SOCKS address types as defined in RFC 1928 section 5.
@@ -45,6 +46,8 @@ const (
 	SHADOWQUIC
 	INNER
 )
+
+var SocksPreferHost atomic.Bool
 
 type AddrType byte
 
@@ -253,6 +256,18 @@ func (m *Metadata) SourceValid() bool {
 }
 
 func (m *Metadata) AddrType() AddrType {
+	if !SocksPreferHost.Load() {
+		switch true {
+		case m.DstIP.Is4():
+			return AtypIPv4
+		case m.DstIP.Is6():
+			return AtypIPv6
+		case m.Host != "" || !m.DstIP.IsValid():
+			return AtypDomainName
+		default:
+			return AtypIPv6
+		}
+	}
 	switch true {
 	case m.Host != "" || !m.DstIP.IsValid():
 		return AtypDomainName
@@ -304,6 +319,15 @@ func (m *Metadata) UDPAddr() *net.UDPAddr {
 }
 
 func (m *Metadata) String() string {
+	if !SocksPreferHost.Load() {
+		if m.DstIP.IsValid() {
+			return m.DstIP.String()
+		} else if m.Host != "" {
+			return m.Host
+		} else {
+			return "<nil>"
+		}
+	}
 	if m.Host != "" {
 		return m.Host
 	} else if m.DstIP.IsValid() {
